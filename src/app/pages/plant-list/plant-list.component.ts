f65c1d085ce3a400b317cdfca0e6ab386ea92e8c
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2022-04-30 12:49:09 -0400

    REFACTOR: change behavior plant list component, to show data from MockPlantServices

--- src/app/pages/plant-list/plant-list.component.ts
+++ src/app/pages/plant-list/plant-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Plant } from 'src/app/classes/plant';
+import { MockPlantsService } from 'src/app/mocks/plants.mocks.service';
 import { PlantsService } from 'src/app/services/plants.service';
 
 @Component({
@@ -12,10 +13,14 @@ export class PlantListComponent implements OnInit {
   plants: Plant[] = [];
   loading: boolean = true;
 
-  constructor(private _plantSrv: PlantsService) { }
+  constructor(
+    private _plantSrv: PlantsService,
+    private _plantMockSrv: MockPlantsService
+  ) { }
 
   ngOnInit(): void {
-    this._plantSrv.getPlants()
+    // this._plantSrv.getPlants() //Uncomment that line to use the API 
+    this._plantMockSrv.getPlants()
       .subscribe({
         next: result => this.plants.push(...result),
         complete: () => this.loading = false
