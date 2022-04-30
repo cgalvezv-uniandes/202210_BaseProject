9eb07295eafad78f211c212ba2dbf9bd4da1fe75
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2022-04-30 11:40:30 -0400

    FEATURE: Add plant service with http request and api url in environments

--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Plant } from '../classes/plant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantsService {
+  private _apiURL: string = environment.apiURL;
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  getPlants(): Observable<Plant[]> {
+    return this._http.get<Plant[]>(this._apiURL).pipe(delay(1000)); //Force delay to show loading process
+  }
 }
